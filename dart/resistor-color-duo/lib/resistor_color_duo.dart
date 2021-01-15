af86e48faf78613fe96edd256ea431baf9db6393
Dmitry Malyshev <damalyshev@gmail.com>
2021-01-15 21:22:33 +0300

    .

---
+++ dart/resistor-color-duo/lib/resistor_color_duo.dart
@@ -0,0 +1,28 @@
+class ResistorColorDuo {
+  final _colors = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+  ];
+
+  int _toNumber(String color) {
+    var index = _colors.indexOf(color);
+    if (index == -1) throw ArgumentError('Unsupported color $color!');
+    return index;
+  }
+
+  int value(List<String> colors) {
+    return int.parse(colors
+        .take(2)
+        .map(_toNumber)
+        .map((number) => number.toString())
+        .join());
+  }
+}
